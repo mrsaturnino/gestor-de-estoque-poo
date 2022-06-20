be5b57d2ef5a839dd0364841a9ff73c39d8e7c27
André Saturnino <andre_santos2012@hotmail.com>
2022-06-20 13:13:10 -0300

    Projeto 3 - Gestor de Estoque

---
+++ gestor-de-estoque/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestor_de_estoque
+{
+    abstract class Produto //Classe pai para todos os tipos de produtos que virão a serem criados.
+        //É uma classe abstrata porque ela simplesmente vai ser uma base para a criação das outras. Classes abstratas não podem ser instanciadas.
+    {
+        public string nome;
+        public float preco;
+    }
+}
---
+++ gestor-de-estoque/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestor_de_estoque
+{
+    class Program
+    {
+        enum Menu { Listar = 1, Adicionar, Remover, Entrada, Saida, Sair}
+        static void Main(string[] args)
+        {
+            bool escolheuSair = false;
+            while (!escolheuSair) //ENQUANTO o "while" for verdadeiro, o programa será executado.
+            {
+                Console.WriteLine("[ SISTEMA DE ESTOQUE ]");
+                Console.WriteLine("1 - Listar\n2 - Adicionar\n3 - Remover\n4 - Registrar entrada\n5 - Registrar saída\n6 - Sair");
+
+                //Capturando a opção do menu escolhida pelo usuário.
+                string opStr = Console.ReadLine();
+                int opInt = int.Parse(opStr);
+
+                if (opInt > 0 && opInt < 7) //Validação da escolha no menu
+                {
+                    Menu escolha = (Menu)opInt; //Casting para converter o numero digitado em uma opção do menu que será interpretada no switch abaixo.
+
+                    switch (escolha)
+                    {
+                        case Menu.Listar:
+                            break;
+                        case Menu.Adicionar:
+                            break;
+                        case Menu.Remover:
+                            break;
+                        case Menu.Entrada:
+                            break;
+                        case Menu.Saida:
+                            break;
+                        case Menu.Sair:
+                            escolheuSair = true;
+                            break;
+                    }
+                }
+                else
+                {
+                    escolheuSair = true;
+                }
+
+                
+            }
+        }
+    }
+}
